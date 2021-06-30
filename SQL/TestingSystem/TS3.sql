646c3af5b5e722581becb7b36dfa6d2e4b2a6cbc
nguyennguyenloc <nnloc123@gmail.com>
2021-06-30 21:49:04 +0700

    TestingSystem3

--- SQL/TestingSystem/TS3.sql
+++ SQL/TestingSystem/TS3.sql
@@ -14,3 +14,23 @@ SELECT * FROM Exam;
 SELECT * FROM ExamQuestion;
 
 -- Câu 2
+SELECT DepartmentName FROM Department;
+
+-- Câu 3
+SELECT DepartmentId, DepartmentName FROM Department WHERE DepartmentName = 'Kế toán';
+
+-- Câu 4
+SELECT * FROM `Account`;
+SELECT MAX(character_length(FullName)) FROM `Account`;
+SELECT FullName FROM `Account` WHERE character_length(FullName) = (SELECT MAX(character_length(FullName)) FROM `Account`);
+
+-- Câu 5
+SELECT FullName FROM `Account` WHERE DepartmentId = 1 AND character_length(FullName) = (SELECT MAX(character_length(FullName)) FROM `Account`);
+
+-- Câu 6
+SELECT * FROM `Group`;
+SELECT GroupName FROM `Group` WHERE CreateDate > '2021-05-30 21:00:11';
+
+-- Câu 7
+SELECT * FROM `Question`;
+SELECT * FROM Answer;
