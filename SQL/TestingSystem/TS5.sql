309cd603762524497e13e8a5d953076a3c3b5a56
nguyennguyenloc <nnloc123@gmail.com>
2021-07-08 22:21:09 +0700

    TestingSystem5

--- SQL/TestingSystem/TS5.sql
+++ SQL/TestingSystem/TS5.sql
@@ -1,39 +1,126 @@
--- SELECT  count(DISTINCT isCorrect) FROM Answer;
--- SELECT count(isCorrect)  FROM Answer Group by isCorrect;
--- SET autocommit = 0;
--- DROP TABLE IF EXISTS Department2;
--- CREATE TABLE Department2(
--- 	DepartmentIDNew 	TINYINT ,
---     DepartmentNameNew	VARCHAR(30)
--- );
--- START TRANSACTION;
--- INSERT INTO Department2 
--- SELECT * FROM Department1;
--- select * from Department2;
 
--- COMMIT;
--- UPDATE Department2 
--- SET 
---     DepartmentNameNew = 'edddđacd',
---     DepartmentIDNew = 34;
+use testing_system;
+select * from Department;
+-- Question 1: Tạo view có chứa danh sách nhân viên thuộc phòng ban sale-- 
+CREATE OR REPLACE VIEW Cau1 AS
+    SELECT 
+        A.*, D.DepartmentName
+    FROM
+        `account` A
+            JOIN
+        department D ON A.DepartmentID = D.DepartmentID
+    WHERE
+        D.DepartmentName = 'Phòng Sale';
+        
+SELECT 
+    *
+FROM
+    Cau1; 
+    
+ -- Question 2: Tạo view có chứa thông tin các account tham gia vào nhiều group nhất--    
+CREATE OR REPLACE VIEW Cau2 AS
+    SELECT 
+         A.*, GA.GroupID
+    FROM
+        GroupAccount GA
+            JOIN
+        `Account` A ON GA.AccountID = A.AccountID
+       GROUP BY GA.AccountID
+    HAVING COUNT(GA.AccountID) = (SELECT 
+                MAX(Temp)
+            FROM
+                (SELECT 
+                    AccountID, COUNT(AccountID) AS Temp
+                FROM
+                    GroupAccount
+                GROUP BY AccountID) AS Ao);
+                
+SELECT 
+    *
+FROM
+    Cau2;
+    
+WITH Cau2_Cach2 AS
+ (SELECT 
+         A.*, GA.GroupID, count(GA.AccountID) as num
+    FROM
+        GroupAccount GA
+            JOIN
+        `Account` A ON GA.AccountID = A.AccountID
+       GROUP BY GA.AccountID),
+MaxResult AS 
+( SELECT max(num) FROM Cau2_Cach2)
+   SELECT 
+		*
+	FROM
+		Cau2_Cach2
+	WHERE
+		Cau2_Cach2.num = (SELECT 
+		*
+	FROM
+		MaxResult);
+-- Question 3: Tạo view có chứa câu hỏi có những content quá dài (content quá 300 từ được coi là quá dài) và xóa nó đi --
+SELECT 
+    *
+FROM
+    question;
+insert into question(QuestionID, Content, CategoryID, TypeID, CreatorID, CreateDate)
+values (11,'pppppppppppppppppppppppppppppppppppppppppppppppppppp',5,1,1,'2020-04-05 00:00:00');
 
--- select * from Department2;
--- ROLLBACK;
--- select * from Department2;
+CREATE OR REPLACE VIEW Cau3 AS
+    SELECT 
+        MAX(LENGTH(Content))
+    FROM
+        Question;
 
--- alter table Department2
--- add column DepartmentManager VARCHAR(20) null After DepartmentNameNew;
--- insert into Department2(DepartmentIDNew)
--- values (9),(10);
--- -- ifnull --
--- select DepartmentNameNew, ifnull(DepartmentManager, "hihi") as DepartmentManager from Department2;
--- select DepartmentNameNew, ifnull(DepartmentManager, DepartmentNameNew) as DepartmentManager from Department2;
--- -- coalesce --
--- select DepartmentNameNew, coalesce(DepartmentManager, DepartmentNameNew, "hihi") as DepartmentManager from Department2;
+DELETE FROM Question 
+WHERE
+    LENGTH(Content) = (SELECT 
+        *
+    FROM
+        Cau3);
+SELECT 
+    *
+FROM
+    Question;
 
--- select DepartmentNameNew, coalesce('cột ảo') as Fake_col from Department2;
+-- Question 4: Tạo view có chứa danh sách các phòng ban có nhiều nhân viên nhất --
+CREATE OR REPLACE VIEW Cau4 AS
+    SELECT 
+        A.*, GA.GroupID
+    FROM
+        GroupAccount GA
+            JOIN
+        `Account` A ON GA.AccountID = A.AccountID
+    GROUP BY GA.GroupID
+    HAVING COUNT(GA.GroupID) = (SELECT 
+            MAX(Temp)
+        FROM
+            (SELECT 
+                GroupID, COUNT(GroupID) AS Temp
+            FROM
+                GroupAccount
+            GROUP BY GroupID) AS Ao);
+                
+SELECT 
+    *
+FROM
+    Cau4;
 
--- select ifnull(DepartmentNameNew, 'DepartmentNameNew is null') as DepartmentNameNew,
--- 		ifnull(DepartmentManager, 'DepartmentManager is null') as DepartmentManager,
---         coalesce(DepartmentNameNew, DepartmentManager)
---         from Department2;
+-- Question 5: Tạo view có chứa tất các các câu hỏi do user họ Nguyễn tạo --
+Select * from Question;
+select * from `Account`;
+CREATE OR REPLACE VIEW Cau5 AS
+    SELECT 
+        A.FullName, Q.Content
+    FROM
+        Question Q
+            JOIN
+        `Account` A ON A.AccountID = Q.CreatorID
+    WHERE
+        A.FullName LIKE 'Nguyễn %';
+        
+SELECT 
+    *
+FROM
+    Cau5;
