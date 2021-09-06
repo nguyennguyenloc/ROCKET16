dd6ee1c0e259800ddbf38fcc5acebe4d50e1b31c
nguyennguyenloc <nnloc123@gmail.com>
2021-09-06 12:11:04 +0700

    Spring Lab 3

---
+++ Spring/Rocket16_Account_Demo_Final/src/main/java/com/vti/Rocket16AccountDemoFinalApplication.java
@@ -0,0 +1,13 @@
+package com.vti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rocket16AccountDemoFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rocket16AccountDemoFinalApplication.class, args);
+	}
+
+}
---
+++ Spring/Rocket16_Account_Demo_Final/src/main/java/com/vti/entity/Position.java
@@ -0,0 +1,70 @@
+package com.vti.entity;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Position", catalog = "TestingSystemSpring1")
+public class Position implements Serializable {
+	private static final long serialVersionUID = 1L;
+	@Column(name = "PositionID")
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private short id;
+
+	@Column(name = "PositionName", nullable = false, unique = true)
+	@Enumerated(EnumType.STRING)
+	private PositionName name;
+
+	public enum PositionName {
+		Dev, Test, Scrum_Master, PM
+	}
+
+	@OneToMany(mappedBy = "position", fetch = FetchType.EAGER)
+	List<Account> account;
+
+	public Position() {
+		super();
+	}
+
+	public short getId() {
+		return id;
+	}
+
+	public void setId(short id) {
+		this.id = id;
+	}
+
+	public PositionName getName() {
+		return name;
+	}
+
+	public void setName(PositionName name) {
+		this.name = name;
+	}
+
+	public List<Account> getAccount() {
+		return account;
+	}
+
+	public void setAccount(List<Account> account) {
+		this.account = account;
+	}
+
+	@Override
+	public String toString() {
+		return "Position [id=" + id + ", name=" + name + "]";
+	}
+
+}
