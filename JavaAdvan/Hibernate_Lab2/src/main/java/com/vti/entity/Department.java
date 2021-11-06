4e43488c7f6758c0cb72c805625779cf7b55439b
nguyennguyenloc <nnloc123@gmail.com>
2021-11-06 22:21:33 +0700

    hibernate2.1

--- JavaAdvan/Hibernate_Lab2/src/main/java/com/vti/entity/Department.java
+++ JavaAdvan/Hibernate_Lab2/src/main/java/com/vti/entity/Department.java
@@ -9,8 +9,18 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
 @Entity
 @Table(name = "Department", catalog = "TestingSystemHibernate2")
+@Setter
+@Getter
+@NoArgsConstructor
+@ToString
+
 public class Department implements Serializable {
 	@Column(name = "DepartmentID")
 	@Id
@@ -20,25 +30,25 @@ public class Department implements Serializable {
 	@Column(name = "DepartmentName", length = 30, nullable = false, unique = true)
 	private String name;
 
-	@Override
-	public String toString() {
-		return "Department [id=" + id + ", name=" + name + "]";
-	}
-
-	public short getId() {
-		return id;
-	}
-
-	public void setId(short id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
+//	@Override
+//	public String toString() {
+//		return "Department [id=" + id + ", name=" + name + "]";
+//	}
+//
+//	public short getId() {
+//		return id;
+//	}
+//
+//	public void setId(short id) {
+//		this.id = id;
+//	}
+//
+//	public String getName() {
+//		return name;
+//	}
+//
+//	public void setName(String name) {
+//		this.name = name;
+//	}
 
 }
