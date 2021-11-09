b58d0018bc6771b76e56b74170493e428f8bd44f
nguyennguyenloc <nnloc123@gmail.com>
2021-11-09 09:44:31 +0700

    hibernate3

---
+++ JavaAdvan/Hibernate_Lab3/src/main/java/com/vti/entity/Department.java
@@ -0,0 +1,29 @@
+package com.vti.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "Department", catalog = "TestingSystemHibernate2")
+@Setter
+@Getter
+@NoArgsConstructor
+@ToString
+public class Department {
+	@Column(name = "DepartmentID")
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private short id;
+
+	@Column(name = "DepartmentName", length = 30, nullable = false, unique = true)
+	private String name;
+}
